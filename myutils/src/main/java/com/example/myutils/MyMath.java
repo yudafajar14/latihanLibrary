b984a7bacec04eff77633a38d29f17274ec80f81
TheQyl01 <yudafajar48@gmail.com>
2019-12-11 14:10:25 +0700

    first
    first

---
+++ myutils/src/main/java/com/example/myutils/MyMath.java
@@ -0,0 +1,26 @@
+package com.example.myutils;
+
+public class MyMath {
+
+    public static int Plus(int a, int b) {
+        return a + b;
+    }
+
+    public static int Minus(int a, int b) {
+        return a - b;
+    }
+
+    public static int Times(int a, int b) {
+        return a * b;
+    }
+
+    public static float Div(int a, int b) {
+
+        if (b == 0) {
+            throw new IllegalArgumentException("Div by Zerro Error");
+        } else {
+            return a / b;
+        }
+
+    }
+}
